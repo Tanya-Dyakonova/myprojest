c34ca2a4170d885d835060f2358e85c0a53db86e
Tanya-Dyakonova <ttdiakonova@yandex.ru>
2021-06-06 09:02:34 +0300

    fourth commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() 
 {
-	int a, b, sum, mult;
+	int a, b, sum, mult, diff;
 	
 	cout << "Input, please, a and b: " << endl;
 	cin>>a>>b;
@@ -13,6 +13,9 @@ int main()
 	
 	mult = a*b;
 	cout << "Mult = " << mult;
+	
+	diff = a - b;
+	cout << "Sum = " << diff;
 	
     return 0;
 }
